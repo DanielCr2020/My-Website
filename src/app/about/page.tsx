73309540c25ca5d2d5fadef3d5e13ba8d97b40a9
UnrealOG <creepercraft137@gmail.com>
2023-08-29 16:36:27 -0400

    Fixed apostraphe issue

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="main">
       <div className="description">
-        <p>You've reached the about page! My name is still Daniel Craig!</p>
+        <p>You&apos;ve reached the about page! My name is still Daniel Craig!</p>
         <p>This page (and the whole website for that matter) is a work in progress!</p>
       </div>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import './styles.css'
+import '@/styles.css'
 import Link from 'next/link'
 
 export default function Home() {

7c5f5c60d1aa66664e04188feb35928ef1d69d3a
DanielCr2020 <danielclass2020@gmail.com>
2024-09-29 00:12:00 -0400

    Updated buttons styling

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -5,12 +5,36 @@ import Link from 'next/link'
 export default function Home() {
   return (
       <header>
-        <Link href="/">Home</Link>
-        <Link href="/projects">Projects</Link><br/>
-        <Link href="/skills">Skills</Link>
-        <Link href="/about">About me</Link>
-        <Link href="https://github.com/DanielCr2020" target="_blank">GitHub</Link><br/>
-        <Link href="https://www.linkedin.com/in/daniel-craig-395085210/" target="_blank">LinkedIn</Link><br/>
+        <a href="/">
+          <button className="link-button"> 
+            Home
+          </button>
+        </a>
+        <a href="/projects">
+          <button className="link-button"> 
+            Projects
+          </button>
+        </a>
+        <a href="/skills">
+          <button className="link-button"> 
+            Skills
+          </button>
+        </a>
+        <a href="/about">
+          <button className="link-button"> 
+            About&nbsp;me
+          </button>
+        </a>
+        <a href="https://github.com/DanielCr2020" target="_blank">
+          <button className="link-button"> 
+            Github
+          </button>
+        </a>
+        <a href="https://www.linkedin.com/in/daniel-craig-395085210/" target="_blank">
+          <button className="link-button">
+            LinkedIn
+          </button>
+        </a>
       </header>
   )
 }

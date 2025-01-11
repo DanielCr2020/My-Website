7eb193e09122519b8fb8f49a34c7041a53b219dd
DanielCr2020 <danielclass2020@gmail.com>
2025-01-10 20:32:27 -0500

    Changed projects page to summary/dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,25 @@ export default function Home() {
       {/* <h1>Website under construction</h1> */}
       <h2 className="title">Hello! My name is Daniel Craig!</h2>
       <div className='description'>
-        <p className="description-text">
+        <p>
           I am a Computer Science major from Stevens Institute of Technology. I graduated with Honors in May of 2024
           with a 3.521 GPA.
           I am interested in pursuing a career in Systems Programming and other lower-level progamming fields.
           This includes but is not limited to Operating System development, driver development, programming for 
           embedded systems, and GPU programming.
         </p>
-        <p className='description-text'>
+        <p>
           As of Fall of 2024, I am pursuing my graduate Computer Science education at Rutgers 
           University - New Brunswick.
         </p>
-        <p className="description-text">
+        <p>
           I also enjoy programming on a slightly higher level, such as game development, web development, and desktop
           app development.
         </p>
-        <p className="description-text">
+        <p>
           This website is a simple NextJS 13 project with simple CSS. It is hosted on Vercel.
         </p>
-        <p className="description-text">
+        <p>
           To learn more, please check out the other pages by clicking the buttons on the top of the page.
           The GitHub and LinkedIn buttons open links in new tabs.
         </p>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,34 +5,31 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="main">
-      <p className='description-text'>
-        Many of these projects can be found on my GitHub
-      </p>
+      <h2>Listed here are various projects I have worked on</h2>
 
-      <div className="side-by-side">
-        <h3 className='project-h3-width'>LS</h3>
+      <details className='large-text'>
+        <summary className="bold-large-text">LS</summary>
         <p className='project-p-width'>
-          An implementation of LS in C with some of the flags.
+          An implementation of the GNU LS coreutil in C with some of the flags. 
+          Uses linux system calls and has proper width printing for long list format.
+          The goal is to have this reimplementation function similarly to the coreutil implementation.
         </p>
+        <p className="project-header">Started:</p>
+        03/2024
         <Link href="https://github.com/DanielCr2020/LS" target="_blank">
           GitHub
         </Link>
-      </div>
-
-      <div className="side-by-side">
-        <h3 className="project-h3-width">Anti-Copy-Paster-Python</h3>
+      </details>
+      <br/>
+      <details className="large-text">
+        <summary className="bold-large-text">Anti-Copy-Paster-Python</summary>
         <p className='project-p-width'>
           A plugin for PyCharm that suggests refactors for duplicated code with customizable triggers.
         </p>
         <Link href="https://github.com/SE4AIResearch/AntiCopyPaster_Python_Fall2023" target="_blank">
           GitHub
         </Link>
-      </div>
-
-
-      <h2>
-
-      </h2>
+      </details>
 
     </main>
   )

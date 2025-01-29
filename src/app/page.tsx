ca0b7af7a1136cb6d3172d685b350a9507466719
DanielCr2020 <danielclass2020@gmail.com>
2025-01-29 11:14:39 -0500

    Removed big name at top of page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className="main">
       {/* <h1>Website under construction</h1> */}
-      <h2 className="title">Hello! My name is Daniel Craig!</h2>
       <div className='description'>
         <p>
           I am a Computer Science major from Stevens Institute of Technology. I graduated with Honors in May of 2024
@@ -16,7 +15,7 @@ export default function Home() {
           embedded systems, and GPU programming.
         </p>
         <p>
-          As of Fall of 2024, I am pursuing my graduate Computer Science education at Rutgers 
+          Starting in the Fall of 2024, I am pursuing my graduate Computer Science education at Rutgers 
           University - New Brunswick.
         </p>
         <p>
@@ -27,7 +26,7 @@ export default function Home() {
           This website is a simple NextJS 13 project with simple CSS. It is hosted on Vercel.
         </p>
         <p>
-          To learn more, please check out the other pages by clicking the buttons on the top of the page.
+          To learn more, feel free to check out the other pages by clicking the buttons on the top of the page.
           The GitHub and LinkedIn buttons open links in new tabs.
         </p>
       </div>
